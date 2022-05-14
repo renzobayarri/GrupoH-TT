3d6023b37496051b786a4680369fa659968d89c2
Renzo Bayarri <renzo.bayarri@alumnos.frm.utn.edu.ar>
2022-05-14 19:40:49 -0300

    Agrega imágenes y referencia a las mismas en la clase Pieza
    Signed-off-by: Renzo Bayarri <renzo.bayarri@alumnos.frm.utn.edu.ar>

--- Proyecto/Piezas/Pieza.py
+++ Proyecto/Piezas/Pieza.py
@@ -1,17 +1,17 @@
 class Pieza:
     images = {
-        "torre_blanca": "",
-        "torre_negra": "",
-        "caballo_blanco": "",
-        "caballo_negro": "",
-        "alfil_blanco": "",
-        "alfil_negro": "",
-        "reina_blanca": "",
-        "reina_negra": "",
-        "rey_blanco": "",
-        "rey_negro": "",
-        "peon_blanco": "",
-        "peon_negro": "",
+        "torre_blanca": "../assets/wRook.png",
+        "torre_negra": "../assets/bRook.png",
+        "caballo_blanco": "../assets/wKnight.png",
+        "caballo_negro": "../assets/bKnight.png",
+        "alfil_blanco": "../assets/wBishop.png",
+        "alfil_negro": "../assets/bBishop.png",
+        "reina_blanca": "../assets/wQueen.png",
+        "reina_negra": "../assets/bQueen.png",
+        "rey_blanco": "../assets/wKing.png",
+        "rey_negro": "../assets/bKing.png",
+        "peon_blanco": "../assets/wPawn.png",
+        "peon_negro": "../assets/bPawn.png",
     }
 
     def __init__(self, is_white):
